d447a6487d5e68259aa04ab034162dc9d4bf442a
MostafizurRahman199 <fardilshifat199@gmail.com>
2024-06-02 21:45:23 +0600

    search component created and api call using useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import Search from './components/Search'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      
+      <Search></Search>
     </div>
   )
 }
---
+++ src/components/Search.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+
+export default function Search() {
+  const url = "https://api.spoonacular.com/recipes/complexSearch";
+  const apiKey = "e3991f8448fc47c3a68cbc5af9e729bd";
+  const [query, setQuery] = useState("pizza");
+
+  useEffect(() => {
+
+    async function  fetchFood() {
+      const response = await fetch(`${url}?query=${query}&apiKey=${apiKey}`);
+     const data = await response.json();
+      console.log(data);
+    }
+
+   fetchFood();
+  }, [query]);
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={query}
+        onChange={(e) => {
+          setQuery(e.target.value);
+        }}
+        className="border rounded-lg focus:ring-2 outline-none text-2xl"
+        placeholder="Enter Food Name"
+      />
+    </div>
+  );
+}
